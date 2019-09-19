08d67ef5f48cd7e88b9aab0388a293677cbb8dbd
ersin <ecyln26@gmail.com>
2019-09-19 20:45:53 +0300

    init base product class

---
+++ app/src/main/java/com/ersinceylan/androidchallenge/Product.java
@@ -0,0 +1,79 @@
+package com.ersinceylan.androidchallenge;
+
+public class Product
+{
+    private int date;
+    private int month;
+    private String oderName;
+    private String marketName;
+    private String orderDetail;
+    private float productPrice;
+    private String productState;
+    private String summaryPrice;
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public String getMarketName() {
+        return marketName;
+    }
+
+    public void setMarketName(String marketName) {
+        this.marketName = marketName;
+    }
+
+    public String getOderName() {
+        return oderName;
+    }
+
+    public void setOderName(String oderName) {
+        this.oderName = oderName;
+    }
+
+    public float getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(float productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductState() {
+        return productState;
+    }
+
+    public void setProductState(String productState) {
+        this.productState = productState;
+    }
+
+    public String getOrderDetail() {
+        return orderDetail;
+    }
+
+    public void setOrderDetail(String orderDetail) {
+        this.orderDetail = orderDetail;
+    }
+
+    public String getSummaryPrice() {
+        return summaryPrice;
+    }
+
+    public void setSummaryPrice(String summaryPrice) {
+        this.summaryPrice = summaryPrice;
+    }
+
+
+}
