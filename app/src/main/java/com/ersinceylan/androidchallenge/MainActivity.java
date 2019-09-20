0ddd0a687a159872dbd3214b991361f09fce679b
ersin <ecyln26@gmail.com>
2019-09-20 23:37:36 +0300

    activity selection was added by rememberMe

--- app/src/main/java/com/ersinceylan/androidchallenge/MainActivity.java
+++ app/src/main/java/com/ersinceylan/androidchallenge/MainActivity.java
@@ -1,13 +1,42 @@
 package com.ersinceylan.androidchallenge;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ersinceylan.androidchallenge.model.Product;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+
+        final SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        if(!sharedPref.getBoolean("rememberMe",false)){
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        else
+            startActivity(new Intent(this, ProductActivity.class));
+
+
+
     }
+
+
+
 }
