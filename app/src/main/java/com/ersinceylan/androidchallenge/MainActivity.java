f25267df4a13430196ffd6e1cfa4fcb666c59ffc
ersin <ecyln26@gmail.com>
2019-09-22 01:30:28 +0300

    deleted unnecessary libraries and added comment

--- app/src/main/java/com/ersinceylan/androidchallenge/MainActivity.java
+++ app/src/main/java/com/ersinceylan/androidchallenge/MainActivity.java
@@ -5,28 +5,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.ersinceylan.androidchallenge.model.Product;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     *  Beni hatırla verisinin değerine göre açılacak activity karar verilir.
+     *  rememberMe=true ise ProductActivity çalışır
+     *  rememberMe=false ise LoginActivity çalışır
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         SharedPreferences sharedPref = this.getSharedPreferences("sharedPref",Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
 
         if(sharedPref.getBoolean("rememberMe",false)){
             startActivity(new Intent(this, ProductActivity.class));
@@ -37,9 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         finish();
 
-
     }
-
-
 
 }
