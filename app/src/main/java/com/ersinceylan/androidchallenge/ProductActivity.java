f57c2f1026dd863ef833f764d22378c62f5817b1
ersin <ecyln26@gmail.com>
2019-09-21 15:16:51 +0300

    Product JSON was fetched in AsyncTask

--- app/src/main/java/com/ersinceylan/androidchallenge/ProductActivity.java
+++ app/src/main/java/com/ersinceylan/androidchallenge/ProductActivity.java
@@ -1,7 +1,14 @@
 package com.ersinceylan.androidchallenge;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.ersinceylan.androidchallenge.model.Product;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class ProductActivity extends AppCompatActivity {
 
@@ -9,5 +16,71 @@ public class ProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        new fetchProductJSON().execute();
+
     }
+
+    public class fetchProductJSON extends AsyncTask<Void, Void, Void> {
+
+        private Product[] products;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                String json = getFromUrl("http://kariyertechchallenge.mockable.io/");
+
+                GsonBuilder builder = new GsonBuilder();
+                builder.setPrettyPrinting();
+                Gson gson = builder.create();
+                products = gson.fromJson(json, Product[].class);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(Void result) {
+            super.onCancelled(result);
+        }
+
+    }
+
+    private static String getFromUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+    }
+
 }
