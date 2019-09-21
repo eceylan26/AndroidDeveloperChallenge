9cf767270e309946b4bc9bad16d74145fc0beb91
ersin <ecyln26@gmail.com>
2019-09-21 15:14:49 +0300

    added class ProductDetail Class for process of gson json to class

---
+++ app/src/main/java/com/ersinceylan/androidchallenge/model/ProductDetail.java
@@ -0,0 +1,24 @@
+package com.ersinceylan.androidchallenge.model;
+
+public class ProductDetail {
+
+    private String orderDetail;
+    private float summaryPrice;
+
+    public String getOrderDetail() {
+        return orderDetail;
+    }
+
+    public void setOrderDetail(String orderDetail) {
+        this.orderDetail = orderDetail;
+    }
+
+    public float getSummaryPrice() {
+        return summaryPrice;
+    }
+
+    public void setSummaryPrice(float summaryPrice) {
+        this.summaryPrice = summaryPrice;
+    }
+
+}
