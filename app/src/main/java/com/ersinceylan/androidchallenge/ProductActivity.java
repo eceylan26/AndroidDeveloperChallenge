1c9f26bf79114e3d771209fa66d14bdc7043dcc3
ersin <ecyln26@gmail.com>
2019-09-22 17:08:04 +0300

    adapter was added to manage reyclerview

--- app/src/main/java/com/ersinceylan/androidchallenge/ProductActivity.java
+++ app/src/main/java/com/ersinceylan/androidchallenge/ProductActivity.java
@@ -1,27 +1,84 @@
 package com.ersinceylan.androidchallenge;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+
+import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
+import com.ersinceylan.androidchallenge.Adapter.ProductAdapter;
 import com.ersinceylan.androidchallenge.model.Product;
+import com.ersinceylan.androidchallenge.model.ProductChild;
+import com.ersinceylan.androidchallenge.model.ProductParent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ProductActivity extends AppCompatActivity {
+
+    @BindView(R.id.product_recycler) RecyclerView product_recycler;
+    @BindView(R.id.orders) Button buttonOrders;
+    @BindView(R.id.logout) Button buttonLogout;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ((ProductAdapter)product_recycler.getAdapter()).onSaveInstanceState(outState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        ButterKnife.bind(this);
 
-        new fetchProductJSON().execute();
+        fetchProductJSON fetchProduct = new fetchProductJSON();
+        fetchProduct.execute();
+
+        product_recycler.setLayoutManager(new LinearLayoutManager(this));
+
+        ProductAdapter adapter = null;
+        try {
+            adapter = new ProductAdapter(this,initData(fetchProduct.products));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        adapter.setParentClickableViewAnimationDefaultDuration();
+        adapter.setParentAndIconExpandOnClick(true);
+
+        product_recycler.setAdapter(adapter);
 
     }
 
-    public class fetchProductJSON extends AsyncTask<Void, Void, Void> {
+    @OnClick(R.id.logout)
+    public void submit() {
+        SharedPreferences sharedPref = this.getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.remove("rememberMe");
+        editor.putBoolean("rememberMe",false);
+        editor.apply();
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+
+    }
+
+    public class fetchProductJSON extends AsyncTask<Void, Void, Product[]> {
 
         private Product[] products;
 
@@ -31,7 +88,7 @@ public class ProductActivity extends AppCompatActivity {
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Product[] doInBackground(Void... params) {
 
             try {
                 String json = getFromUrl("http://kariyertechchallenge.mockable.io/");
@@ -40,16 +97,16 @@ public class ProductActivity extends AppCompatActivity {
                 builder.setPrettyPrinting();
                 Gson gson = builder.create();
                 products = gson.fromJson(json, Product[].class);
-
+                int a =0;
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            return null;
+            return products;
         }
 
         @Override
-        protected void onPostExecute(Void result) {
+        protected void onPostExecute(Product[] result) {
             super.onPostExecute(result);
         }
 
@@ -59,7 +116,7 @@ public class ProductActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onCancelled(Void result) {
+        protected void onCancelled(Product[] result) {
             super.onCancelled(result);
         }
 
@@ -82,5 +139,53 @@ public class ProductActivity extends AppCompatActivity {
                 reader.close();
         }
     }
+
+    private List<ParentObject> initData(Product[] productsArray) throws ParseException {
+
+        List<ParentObject> listParentProducts = new ArrayList<>();
+
+        Arrays.sort(productsArray);
+        Product previousProduct=null;
+
+        for (Product var : productsArray) {
+
+            ProductParent productParent = new ProductParent();
+
+            productParent.setMarketName(var.getMarketName());
+            productParent.setOrderName(var.getOrderName());
+            productParent.setProductPrice(var.getProductPrice() + "TL");
+            productParent.setProductState(var.getProductState());
+
+            /**
+             * Bu if check aynı ay ve gün ürünlerin tarihlerinin görünmesinin engeller
+             */
+            if(previousProduct!=null && previousProduct.getMonth().equals(var.getMonth())
+                    && previousProduct.getDate().equals(var.getDate())) {
+                productParent.setMonth("");
+                productParent.setDate("");
+            }else {
+                productParent.setMonth(var.getFormatMonth());
+                productParent.setDate(var.getDate());
+            }
+            List<Object> childList = new ArrayList<>();
+            ProductChild productChild = new ProductChild();
+
+            productChild.setName(var.getProductDetail().getOrderDetail());
+            productChild.setPrice(var.getProductDetail().getSummaryPrice() + "TL");
+
+            childList.add(productChild);
+            productParent.setChildObjectList(childList);
+
+            listParentProducts.add(productParent);
+
+            previousProduct = var;
+        }
+
+        return listParentProducts;
+
+    }
+
+
+
 
 }
